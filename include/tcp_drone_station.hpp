52b7a3fa4d03c9b8362607a81096e5a226e31d77
LemperorD <ld2382619813@163.com>
2025-09-26 13:18:05 +0800

    add the hpp

---
+++ include/tcp_drone_station.hpp
@@ -0,0 +1,77 @@
+#ifndef TCP_DRONE_STATION_HPP
+#define TCP_DRONE_STATION_HPP
+
+#include <ros/ros.h>
+
+// 基础消息
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
+#include <visualization_msgs/MarkerArray.h>
+
+// MAVROS 专用
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/BatteryStatus.h>
+#include <mavros_msgs/WaypointList.h>
+#include <mavros_msgs/WaypointPush.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/CommandHome.h>
+#include <mavros_msgs/CommandLong.h>
+
+
+using namespace std;
+
+// ---------------- 工具函数 ----------------
+inline int remap_ground_station_id(int id) {
+  return id + drone_num_;
+}
+
+template <typename T>
+int send_to_all_drone_except_me(const string &topic, const T &msg) {
+  int err_code = 0;
+  for (int i = 0; i < drone_num_; ++i) {
+    if (i == self_id_in_bridge_) continue;
+    err_code += bridge->send_msg_to_one(i, topic, msg);
+  }
+  return err_code;
+}
+
+template <typename T>
+int send_to_all_groundstation_except_me(const string &topic, const T &msg) {
+  int err_code = 0;
+  for (int i = 0; i < ground_station_num_; ++i) {
+    int ind = remap_ground_station_id(i);
+    if (ind == self_id_in_bridge_) continue;
+    err_code += bridge->send_msg_to_one(ind, topic, msg);
+  }
+  return err_code;
+}
+
+void register_callback_to_all_groundstation(string topic_name, function<void(int, ros::SerializedMessage &)> callback)
+{
+  for (int i = 0; i < ground_station_num_; ++i)
+  {
+    int ind = remap_ground_station_id(i);
+    if (ind == self_id_in_bridge_)  //skip myself
+    {
+      continue;
+    }
+    bridge->register_callback(ind,topic_name,callback);
+  }
+}
+
+void register_callback_to_all_drones(string topic_name, function<void(int, ros::SerializedMessage &)> callback)
+{
+  for (int i = 0; i < drone_num_; ++i)
+  {
+    if (i == self_id_in_bridge_)  //skip myself
+    {
+      continue;
+    }
+    bridge->register_callback(i,topic_name,callback);
+  }
+}
+
+#endif 
---
+++ src/tcp_station.cpp
@@ -0,0 +1,54 @@
+#include "reliable_bridge.hpp"
+#include "tcp_drone_station.hpp"
+
+int self_id_;
+int self_id_in_bridge_;
+int drone_num_;
+int ground_station_num_;
+bool is_groundstation_;
+
+vector<int> id_list_;
+vector<string> ip_list_;
+
+unique_ptr<ReliableBridge> bridge;
+
+void takeoff_command_sub_cb(); // 地面到飞机：起飞指令
+void land_command_sub_cb();    // 地面到飞机：降落或返航指令
+void waypoint_push_sub_cb(); // 地面到飞机：航点下发
+
+void pose_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：位姿
+void vel_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：速度
+void battery_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：电池状态
+void state_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：飞控状态
+void waypoint_list_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：当前航点列表
+void video_bridge_cb(int ID, ros::SerializedMessage &m); // 飞机到地面：视频流
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "swarm_bridge");
+  ros::NodeHandle nh("~");
+
+  // 参数读取
+  nh.param("self_id", self_id_, -1);
+  nh.param("is_ground_station", is_groundstation_, false);
+  nh.param("drone_num", drone_num_, 1);
+  nh.param("ground_station_num", ground_station_num_, 0);
+
+  // 配置 ID & IP
+  id_list_.resize(drone_num_ + ground_station_num_);
+  ip_list_.resize(drone_num_ + ground_station_num_);
+  for (int i = 0; i < drone_num_ + ground_station_num_; ++i) {
+    nh.param((i < drone_num_ ? "drone_ip_" + to_string(i)
+                             : "ground_station_ip_" + to_string(i - drone_num_)),
+             ip_list_[i], string("127.0.0.1"));
+    id_list_[i] = i;
+  }
+
+  self_id_in_bridge_ = self_id_;
+  if (is_groundstation_) self_id_in_bridge_ = remap_ground_station_id(self_id_);
+
+  bridge.reset(new ReliableBridge(self_id_in_bridge_, ip_list_, id_list_, 100000));
+
+  ros::spin();
+  bridge->StopThread();
+  return 0;
+}
